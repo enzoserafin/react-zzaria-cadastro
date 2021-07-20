59922cbb45feb33ed13ed20834aa6336dcb61b20
enzo <enzoserafin@gmail.com>
2021-07-20 16:19:03 -0300

    listagem tamanhos de pizzas

--- src/pages/PizzasSizes/index.js
+++ src/pages/PizzasSizes/index.js
@@ -1,3 +1,51 @@
-const PizzasSizes = () => <h1>Pizzas Sizes</h1>
+import {
+  TableContainer,
+  TableTitle,
+  THead,
+  Th
+} from './styles'
+import {
+  Table,
+  TableBody,
+  TableRow,
+  TableCell
+} from '@material-ui/core'
+import useCollection from '../../hooks/db/collection'
+import singularOrPlural from '../../utils/singularOrPlural'
+
+const PizzasSizes = () => {
+  const pizzasSizes = useCollection('pizzasSizes')
+
+  return (
+    <TableContainer>
+      <TableTitle>
+        Tamanhos cadastrados
+      </TableTitle>
+
+      <Table>
+        <THead>
+          <TableRow>
+            <Th>Nome</Th>
+            <Th>Diâmetro</Th>
+            <Th>Fatias</Th>
+            <Th>Sabores</Th>
+          </TableRow>
+        </THead>
+        <TableBody>
+          {pizzasSizes?.map(pizza => (
+            <TableRow key={pizza.id}>
+              <TableCell>{pizza.name}</TableCell>
+              <TableCell>{pizza.size} cm</TableCell>
+              <TableCell>{pizza.slices} fatias</TableCell>
+              <TableCell>
+                {pizza.flavours} {' '}
+                {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+}
 
 export default PizzasSizes
