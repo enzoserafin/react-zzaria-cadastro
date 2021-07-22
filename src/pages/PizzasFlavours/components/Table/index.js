2e84074cfb306bc897de9a19f6bfad1b0dff9c4d
enzo <enzoserafin@gmail.com>
2021-07-22 14:38:52 -0300

    listagem de sabores

--- src/pages/PizzasFlavours/components/Table/index.js
+++ src/pages/PizzasFlavours/components/Table/index.js
@@ -1,27 +1,25 @@
 import { Link, useRouteMatch } from 'react-router-dom'
-import {
-  PIZZAS_FLAVOURS, NEW
-  // , EDIT
-} from '../../../../routes'
+import t from 'prop-types'
+import { PIZZAS_FLAVOURS, NEW, EDIT } from '../../../../routes'
 import {
   Button,
   TableContainer,
   TableTitle,
-  TitleContainer
-  // THead,
-  // Th
+  TitleContainer,
+  THead,
+  Th
 } from './styles'
 import {
-  Grid
-  // Table,
-  // TableBody,
-  // TableRow,
-  // TableCell
+  Grid,
+  List,
+  ListItem as MaterialListItem,
+  ListItemText,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell
 } from '@material-ui/core'
-import {
-  Add
-  // , Edit, Delete
-} from '@material-ui/icons'
+import { Add, Edit, Delete } from '@material-ui/icons'
 // import useCollection from '../../../../hooks/db/collection'
 // import singularOrPlural from '../../../../utils/singularOrPlural'
 
@@ -50,50 +48,74 @@ const TablePizzasFlavours = () => {
         </Grid>
       </TitleContainer>
 
-      {/* <Table>
+      <Table>
         <THead>
           <TableRow>
+            <Th>Foto</Th>
             <Th>Nome</Th>
-            <Th>Diâmetro</Th>
-            <Th>Fatias</Th>
-            <Th>Sabores</Th>
+            <Th>Valores</Th>
             <Th />
           </TableRow>
         </THead>
         <TableBody>
-          {pizzasSizes?.map(pizza => (
-            <TableRow key={pizza.id}>
-              <TableCell>{pizza.name}</TableCell>
-              <TableCell>{pizza.size} cm</TableCell>
-              <TableCell>{pizza.slices} fatias</TableCell>
-              <TableCell>
-                {pizza.flavours} {' '}
-                {singularOrPlural(pizza.flavours, 'sabor', 'sabores')}
-              </TableCell>
+          <TableRow >
+            <TableCell>
+              <img
+                src=''
+                alt=''
+                style={{
+                  background: '#fc0',
+                  display: 'block',
+                  height: '50px',
+                  width: '50px'
+                }}
+              />
+            </TableCell>
 
-              <TableCell align='right'>
-                <Button
-                  startIcon={<Edit />}
-                  component={Link}
-                  to={PIZZAS_SIZES + EDIT(pizza.id)}
-                >
-                  Editar
-                </Button>
+            <TableCell>Sabor da pizza</TableCell>
+            <TableCell>
+              <List>
+                <ListItem name='Broto' value={10} />
+                <ListItem name='Pequena' value={20} />
+                <ListItem name='Grande' value={30} />
+              </List>
+            </TableCell>
 
-                <Button
-                  color='secondary'
-                  startIcon={<Delete />}
-                  onClick={() => remove(pizza.id)}
-                >
-                  Remover
-                </Button>
-              </TableCell>
-            </TableRow>
-          ))}
+            <TableCell align='right'>
+              <Button
+                startIcon={<Edit />}
+                component={Link}
+                to={PIZZAS_FLAVOURS + EDIT(1)}
+              >
+                Editar
+              </Button>
+
+              <Button
+                color='secondary'
+                startIcon={<Delete />}
+                onClick={() => { }}
+              >
+                Remover
+              </Button>
+            </TableCell>
+          </TableRow>
         </TableBody>
-      </Table> */}
+      </Table>
     </TableContainer>
   )
+}
+
+const ListItem = ({ name, value }) => (
+  <MaterialListItem>
+    <ListItemText>
+      <strong>{name}</strong>: R$ {value}
+    </ListItemText>
+  </MaterialListItem>
+)
+
+ListItem.propTypes = {
+  name: t.string.isRequired,
+  value: t.number.isRequired
 }
 
 export default TablePizzasFlavours
